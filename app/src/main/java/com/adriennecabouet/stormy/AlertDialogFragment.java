e5eaf10f40bec42edd767cf0b7230a9498bfbdc4
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2015-08-30 02:25:26 -0700

    Initial commit for Stormy app

---
+++ app/src/main/java/com/adriennecabouet/stormy/AlertDialogFragment.java
@@ -0,0 +1,24 @@
+package com.adriennecabouet.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Adrienne on 8/24/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_okButtonText, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
